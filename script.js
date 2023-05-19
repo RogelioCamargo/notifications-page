1b1e2970bbf75f71e77412251a2b8f68fc12cc32
Rogelio Camargo <rogelio.camargo2016@gmail.com>
2023-05-19 08:36:01 -0700

    Apply prettier, add event handler to notification post

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ markAllReadButton.addEventListener("click", (e) => {
 
   unreadNotifsCount.classList.add("hidden");
 });
+
+// A possible alterative if we use event delegation:
+// https://stackoverflow.com/questions/36121179/event-delegation-pattern-with-addeventlistener-and-nested-elements-in-buttons
+const notifs = document.querySelectorAll(".notif");
+notifs.forEach((notif) => {
+  notif.addEventListener("click", () => {
+    notif.classList.remove("notif--unread");
+    unreadNotifsCount.textContent =
+      document.querySelectorAll(".notif--unread").length;
+
+    if (unreadNotifsCount.textContent === "0") {
+      unreadNotifsCount.classList.add("hidden");
+    }
+  });
+});
